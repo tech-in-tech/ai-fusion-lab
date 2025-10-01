9b18a3f22f1b70d2899c16688cf1a65501ed3c3d
tech-in-tech <anubhavsharma06dec@gmail.com>
2025-10-01 17:33:29 +0530

    Added Sidebar

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { AppSidebar } from './_components/AppSidebar'
 function Provider({
   children, ...props
 }) {
@@ -8,9 +10,12 @@ function Provider({
       attribute="class"
       defaultTheme="dark"
       enableSystem
-      disableTransitionOnChange
-      {...props}>
-      <div>{children}</div>
+      disableTransitionOnChange>
+      <SidebarProvider>
+        <AppSidebar/>
+        <SidebarTrigger/>
+        <div>{children}</div>
+      </SidebarProvider>
     </NextThemesProvider>
   )
 }
